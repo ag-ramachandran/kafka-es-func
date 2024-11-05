7f3018204af53b2abd6a6332a7563dd677fc89df
Ramachandran G <ramacg@microsoft.com>
2024-11-05 11:24:30 +0530

    * Working sample

--- kafka-es-function-inproc/KafkaToESInproc.cs
+++ kafka-es-function-inproc/KafkaToESInproc.cs
@@ -1,7 +1,12 @@
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Kafka;
+using Microsoft.Azure.WebJobs.Extensions.EventHubs;
 using Microsoft.Extensions.Logging;
 using Avro.Generic;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace KafkaToEventStreamInProcFunctions
 {
@@ -14,7 +19,7 @@ namespace KafkaToEventStreamInProcFunctions
         // "BrokerList": "{EVENT_HUBS_NAMESPACE}.servicebus.windows.net:9093"
         // "KafkaPassword":"{EVENT_HUBS_CONNECTION_STRING}
         [FunctionName("KafkaToESInproc")]
-        public void Run(
+        public async Task Run(
             [KafkaTrigger("%CONNECT_BOOTSTRAP_SERVERS%","%CONNECT_TOPIC%",
                     Protocol = BrokerProtocol.Ssl,
                     SslKeyLocation = "%CONNECT_SSL_KEYSTORE_LOCATION%",
@@ -23,12 +28,27 @@ namespace KafkaToEventStreamInProcFunctions
                     SslCertificateLocation = "%CONNECT_SSL_TRUSTSTORE_LOCATION%",
                     SchemaRegistryUrl = "%CONNECT_SCHEMA_REGISTRY_URL%",                    
                     ConsumerGroup = "%CONNECT_GROUP_ID%"
-                    )] KafkaEventData<GenericRecord>[] events, ILogger log)
+                    )] KafkaEventData<GenericRecord>[] events, 
+                    [EventHub("%EVENTSTREAM_NAME%", Connection = "ES_CONNECTION")]IAsyncCollector<EventData> outputEvents,
+                    ILogger log)
         {
             foreach (KafkaEventData<GenericRecord> eventData in events)
             {
-                log.LogInformation($"C# Kafka trigger function processed a message: {eventData.Value}");
+                var valueRecord = eventData.Value;
+                log.LogInformation($"Key: {eventData.Key}, Value: {valueRecord.ToString()}");
+                await outputEvents.AddAsync(new EventData(GenericToJson(valueRecord)));
             }
         }
+        // From : https://github.com/Azure/azure-functions-kafka-extension/blob/dev/samples/dotnet/KafkaFunctionSample/AvroGenericTriggers.cs
+        private static byte[] GenericToJson(GenericRecord record)
+        {
+            var props = new Dictionary<string, object>();
+            foreach (var field in record.Schema.Fields)
+            {
+                if (record.TryGetValue(field.Name, out var value))
+                    props[field.Name] = value;
+            }
+            return System.Text.Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(props));
+        }        
     }
 }
